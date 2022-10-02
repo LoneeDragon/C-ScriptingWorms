b0c8f4822ece73b448f7baee285f89e65d693361
LoneeDragon <113420286+LoneeDragon@users.noreply.github.com>
2022-10-02 06:31:58 +0200

    no damage or camera follow players

---
+++ C-ScriptingWorms/Assets/Scrips/PlayerCamera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+//[SerializeField] private PlayerTurn manager;
+    [SerializeField] private Vector3 distanceFromThePlayer;
+
+
+    void Update()
+    {
+    //    transform.position = manager.GetCurrentPlayer().transform.position + distanceFromThePlayer;
+    }
+}
